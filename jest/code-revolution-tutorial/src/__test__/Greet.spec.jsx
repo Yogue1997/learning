6971e235c5f2a87095013e3b52fbd9c5d0eb63a0
yyogue <youssouf.e.yogue@aexp.com>
2022-10-13 11:26:22 -0500

    A specific folder for testing has been created and test() has been replaced by it()

---
+++ jest/code-revolution-tutorial/src/__test__/Greet.spec.jsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import Greet from "../components/greet/Greet";
+
+// In this scenario, test() has been replaced by it()
+
+describe("Greet", () => {
+
+  it("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  describe("Nested", () => {
+    it("renders with a name", () => {
+      render(<Greet name="Youssouf" />);
+      const textElement = screen.getByText("Hello Youssouf");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
+});
+
+
+
+
+// describe("Greet", () => {
+  
+//   test("renders correctly", () => {
+//     render(<Greet />);
+//     const textElement = screen.getByText("Hello");
+//     expect(textElement).toBeInTheDocument();
+//   });
+
+//   describe("Nested", () => {
+//     test("renders with a name", () => {
+//       render(<Greet name="Youssouf" />);
+//       const textElement = screen.getByText("Hello Youssouf");
+//       expect(textElement).toBeInTheDocument();
+//     });
+//   });
+// });
